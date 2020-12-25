5f784065971ee269481420c80b5aaf5f771e8686
Michael Nef <michael@nef.net.au>
2020-12-25 17:42:21 +0800

    content block component

---
+++ components/ContentBlock.js
@@ -0,0 +1,53 @@
+import styles from "./ContentBlock.module.css";
+import React, { useState } from "react";
+
+const Description = ({ description }) => {
+  const [truncated, setTruncated] = useState(true);
+
+  if (description.length >= 90) {
+    if (truncated) {
+      const shortDesc = description.slice(0, 90);
+      return (
+        <p>
+          {shortDesc}{" "}
+          <button
+            className={styles.expander}
+            onClick={() => setTruncated(false)}
+          >
+            ...Expand
+          </button>{" "}
+        </p>
+      );
+    } else {
+      return (
+        <p>
+          {description}{" "}
+          <button
+            className={styles.expander}
+            onClick={() => setTruncated(true)}
+          >
+            Collapse
+          </button>
+        </p>
+      );
+    }
+  }
+  return <p>{description}</p>;
+};
+
+const ContentBlock = ({ heading, descriptions }) => {
+  const ContentItems = descriptions.map((desc, idx) => (
+    <li key={idx}>
+      <Description description={desc} />
+    </li>
+  ));
+
+  return (
+    <>
+      <h2 className={styles.subheading}>{heading}</h2>
+      <ul>{ContentItems}</ul>
+    </>
+  );
+};
+
+export default ContentBlock;
