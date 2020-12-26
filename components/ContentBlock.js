8746ce814cb9dd795eb30294ae976858841acc05
Michael Nef <michael@nef.net.au>
2020-12-26 11:50:09 +0800

    removed "Description" component code

--- components/ContentBlock.js
+++ components/ContentBlock.js
@@ -1,40 +1,6 @@
 import styles from "./ContentBlock.module.css";
 import React, { useState } from "react";
 
-const Description = ({ description }) => {
-  const [truncated, setTruncated] = useState(true);
-
-  if (description.length >= 90) {
-    if (truncated) {
-      const shortDesc = description.slice(0, 90);
-      return (
-        <p>
-          {shortDesc}{" "}
-          <button
-            className={styles.expander}
-            onClick={() => setTruncated(false)}
-          >
-            ...Expand
-          </button>{" "}
-        </p>
-      );
-    } else {
-      return (
-        <p>
-          {description}{" "}
-          <button
-            className={styles.expander}
-            onClick={() => setTruncated(true)}
-          >
-            Collapse
-          </button>
-        </p>
-      );
-    }
-  }
-  return <p>{description}</p>;
-};
-
 const ContentBlock = ({ heading, descriptions }) => {
   const [showMore, setShowMore] = useState(false);
 
