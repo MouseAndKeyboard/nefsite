76842667a2d0af3094d9c558fa36738cad9b47cb
Michael Nef <michael@nef.net.au>
2020-12-26 11:46:38 +0800

    added "show more" button to view more bullets

--- components/ContentBlock.js
+++ components/ContentBlock.js
@@ -36,15 +36,40 @@ const Description = ({ description }) => {
 };
 
 const ContentBlock = ({ heading, descriptions }) => {
-  const ContentItems = descriptions.map((desc, idx) => (
-    <li key={idx}>{desc}</li>
-  ));
+  const [showMore, setShowMore] = useState(false);
+
+  const BULLET_COUNT = 5;
+
+  let ContentItems = [];
+  let expander = <></>;
+  if (descriptions.length >= BULLET_COUNT) {
+    const toShow = showMore
+      ? descriptions
+      : descriptions.slice(0, BULLET_COUNT);
+
+    ContentItems = toShow.map((desc, idx) => <li key={idx}>{desc}</li>);
+
+    const word = showMore ? <>Show Less ▴</> : <>Show More ▾</>;
+    expander = (
+      <div className={styles.center}>
+        <button
+          className={styles.expander}
+          onClick={() => setShowMore((prior) => !prior)}
+        >
+          {word}
+        </button>
+      </div>
+    );
+  } else {
+    ContentItems = descriptions.map((desc, idx) => <li key={idx}>{desc}</li>);
+  }
 
   return (
-    <>
+    <div className={styles.contentBlock}>
       <h2 className={styles.subheading}>{heading}</h2>
       <ul className={styles.bullets}>{ContentItems}</ul>
-    </>
+      {expander}
+    </div>
   );
 };
 
