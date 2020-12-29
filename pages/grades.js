34672d233c814fa4d7ecb010e24471e90e98f3ab
Michael Nef <michael@nef.net.au>
2020-12-30 01:31:00 +0800

    Addded extra information to grades page

--- pages/grades.js
+++ pages/grades.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import GradesPage, { createData } from "../components/Grades";
 import React from "react";
+import styles from "../styles/Grades.module.css";
 
 const uniGrades = [
   createData("Software Engineering With Java", "CITS1001", 91, "HD"),
@@ -19,12 +20,25 @@ const uniGrades = [
 ];
 
 const Grades = () => {
+  const reducer = (total, cur) => total + cur.mark;
+  const wam = uniGrades.reduce(reducer, 0) / uniGrades.length;
+
   return (
     <div>
       <Head>
         <title>Grades | Michael Nefiodovas</title>
       </Head>
-      <GradesPage grades={uniGrades} />
+      <main className={styles.main}>
+        <h1 className={styles.title}>Michael Nefiodovas's Grades</h1>
+        <h2 className={styles.subtitle}>
+          University of Western Australia Grades
+        </h2>
+        <p>
+          GPA: 7.0 <span className={styles.weak}>(/7.0)</span> <br /> WAM: {wam}{" "}
+          <span className={styles.weak}>(/100.00)</span>
+        </p>
+        <GradesPage grades={uniGrades} />
+      </main>
     </div>
   );
 };
