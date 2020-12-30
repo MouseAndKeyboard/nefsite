4f6c01cb618ab19d88c6566a873ccd37f95589ce
Michael Nef <michael@nef.net.au>
2020-12-30 10:08:14 +0800

    responsive design on grades page

--- pages/grades.js
+++ pages/grades.js
@@ -28,22 +28,23 @@ const Grades = () => {
       <Head>
         <title>Grades | Michael Nefiodovas</title>
       </Head>
+      <div className={styles.content}>
+        <div className={styles.gohome}>
+          <a href="/">◂ Home</a>
+        </div>
 
-      <div className={styles.gohome}>
-        <a href="/">◂ Home</a>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Michael Nefiodovas's Grades</h1>
+          <h2 className={styles.subtitle}>
+            Grades from The University of Western Australia
+          </h2>
+          <p>
+            GPA: 7.0 <span className={styles.weak}>(/7.0)</span> <br /> WAM:{" "}
+            {wam} <span className={styles.weak}>(/100.00)</span>
+          </p>
+          <GradesPage grades={uniGrades} />
+        </main>
       </div>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Michael Nefiodovas's Grades</h1>
-        <h2 className={styles.subtitle}>
-          Grades from The University of Western Australia
-        </h2>
-        <p>
-          GPA: 7.0 <span className={styles.weak}>(/7.0)</span> <br /> WAM: {wam}{" "}
-          <span className={styles.weak}>(/100.00)</span>
-        </p>
-        <GradesPage grades={uniGrades} />
-      </main>
     </div>
   );
 };
