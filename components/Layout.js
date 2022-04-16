28a6a8817d50075029107e09db408d22a3d9d60a
Michael Nef <michael@nef.net.au>
2022-04-16 23:23:12 +0800

    reverted to old font

--- components/Layout.js
+++ components/Layout.js
@@ -4,12 +4,12 @@ import Head from "next/head";
 const Layout = ({children, title}) => (
     <div>
       <Head>
-          <link
+        {/* <link
             rel="preload"
             href="/fonts/wotford-regular-webfont.ttf"
             as="font"
             crossOrigin=""
-          />
+            /> */}
           <link rel="icon" href="/favicon.ico" />
           <title>{title}</title>
       </Head>
