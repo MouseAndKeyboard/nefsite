1744270aaa9f6b5699e1c82cf400f368beaa34d4
Michael Nef <michael@nef.net.au>
2022-11-24 19:48:52 +0800

    updated grades

--- components/data/pf.js
+++ components/data/pf.js
@@ -2,5 +2,6 @@
 import { createData } from "./grades.js";
 
 export default [
-    createData("BPhil Research Placement", "GCRL2000", "N/A", "Pass")
+    createData("BPhil Research Placement", "GCRL2000", "N/A", "Pass"),
+    createData("BPhil Research Project", "GCRL3000", "N/A", "Pass"),
 ];
